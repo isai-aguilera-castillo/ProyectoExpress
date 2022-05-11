aced724ff7c96ba0acee5d8d1f5729e97e7d5745
ISAI ARON AGUILERA CASTILLO <isaiaron@hotmail.com>
2022-05-11 12:03:28 -0600

    agregar un nuevo usario

--- routes/routes.js
+++ routes/routes.js
@@ -38,5 +38,18 @@ app,get('/users/:id', (request, response) =>{
     });
 });
 
+// Agregar un nuevo usario 
+
+app.post('/users', (request, response) => {
+ 
+    pool.query('INSERT INTO users SET ?', request.body, (error, result) => {
+
+        if (error) throw error; 
+
+        response.status(201).send(`User added with ID: ${result, insertId}`);
+    });
+
+});
+
 
 }
