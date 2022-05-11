15c53fb6f2c851bbafdc77a23d4d4c6d4bf21906
ISAI ARON AGUILERA CASTILLO <isaiaron@hotmail.com>
2022-05-11 11:12:50 -0600

    codigo del puerto 3002

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 var express = require ('express');
 var app = express ();
 
@@ -12,3 +13,29 @@ app.get ('/adios', function (req, res){
 app.listen(3000, function(){
     console.log('Aplicacion ejemplo. escuchando el puerto 3000!');
 });
+
+*/
+
+//requiere packages and set the port
+const express = require ('express');
+const port = 3002;
+// para permitir el manejo de POST y PUT 
+
+const bodyParser = require ('body-parser');
+const routes = require ('./routes/routes')
+const app = express (); 
+
+//usar Node.js body parsing middleware 
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({
+     extended: true,
+ })); 
+
+ routes(app);
+ //inciiar servidor 
+
+ const server = app.listen(port, (error)=>{
+        if (error) return console.log(`Error: ${error}`);
+        console.log(`El servidor escucha en el puerto ${server.address().port}`);
+
+ });
