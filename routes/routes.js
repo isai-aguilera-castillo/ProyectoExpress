840e514a8a33391feb97c5ca7454b4e9a3b5e9e6
ISAI ARON AGUILERA CASTILLO <isaiaron@hotmail.com>
2022-05-12 11:42:37 -0600

    eliminar usario y exportar  router

--- routes/routes.js
+++ routes/routes.js
@@ -61,12 +61,23 @@ app.put ('/users/:id', (request, response) => {
     response.send('User updated successfully.');
   });
 
-
-
-
 });
 
 
 
+//eliminar un usario 
+app.delete('/users/:id', (request, response) => {
+    const id = request.params.id;
+    pool.query('DELETE FROM users WHERE id = ?', id, (error, result) => {
+
+       if (error) throw error;
+       response.send('User deleted');
+
+    });
+});
+
 
 }
+
+//Exportortar el router 
+module.exports = router;
