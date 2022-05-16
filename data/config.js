75982507ce1528938de1579606c03043e5d69cb4
ISAI ARON AGUILERA CASTILLO <isaiaron@hotmail.com>
2022-05-16 03:16:12 -0600

    marge config windows

--- data/config.js
+++ data/config.js
@@ -1,20 +1,28 @@
+const sql = require('mssql');
 
-
-const mysql = require('mysql');
-
-// set database connection credentials 
-
+//Set database conecction credentials
 const config = {
-    host: 'localhost',
-    user: 'root',
-    password: 'Castillo20$', 
-    database: 'API'
+    server: 'localhost',
+    user: 'aguilera',
+    password: 'qwerty',
+    database: 'api',
+    options: {
+        trustServerCertificate: true
+    }
 };
 
-//ctreate a my sql pool
+//create a MSSQL pool
+const pool = {
+    query: function(query, callback) {
+        sql.connect(config).then((pool) => {
+            return pool.query(query);
+        }).then(result => {
+            callback(null, result);
+        }).catch(err => {
+            callback(err, null);
+        });
+    }
+}
 
-const pool = mysql.createPool(config);
-
-//export the pool 
-
-module.exports = pool;
+// Export the pool
+module.exports=pool;
