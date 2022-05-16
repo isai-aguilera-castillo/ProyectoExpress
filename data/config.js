6a419380ced4ba447d65937a2548c84d1541715e
ISAI ARON AGUILERA CASTILLO <isaiaron@hotmail.com>
2022-05-16 03:08:29 -0600

    Windows server

--- data/config.js
+++ data/config.js
@@ -1,5 +1,5 @@
 
-
+/*
 const mysql = require('mysql');
 
 // set database connection credentials 
@@ -18,3 +18,34 @@ const pool = mysql.createPool(config);
 //export the pool 
 
 module.exports = pool;
+
+*/
+
+const sql = require('mssql');
+
+//Set database conecction credentials
+const config = {
+    server: 'localhost',
+    user: 'aguilera',
+    password: 'qwerty',
+    database: 'api',
+    options: {
+        trustServerCertificate: true
+    }
+};
+
+//create a MSSQL pool
+const pool = {
+    query: function(query, callback) {
+        sql.connect(config).then((pool) => {
+            return pool.query(query);
+        }).then(result => {
+            callback(null, result);
+        }).catch(err => {
+            callback(err, null);
+        });
+    }
+}
+
+// Export the pool
+module.exports=pool;
